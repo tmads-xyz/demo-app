715a1d14a7fbece0f55f529d435ad7c5d1d05286
filippgit <35934233+filippgit@users.noreply.github.com>
2024-05-11 15:33:46 +0300

    added Interstitial and Rewarded ads

--- example-app.js
+++ example-app.js
@@ -16,6 +16,38 @@ window.onload=function(){ //initializing the whole thing on page load
     document.getElementById("bottom").style.height="40px";
     TMAds.hide();
   };
+  document.getElementById("showInt").onclick=function(){ //show TMAds Interstital upon clicking "Show" button
+    this.disabled=true;
+    document.getElementById("title").innerHTML="Showing Interstital...";
+    document.getElementById("preloadInt").disabled=true;
+    document.getElementById("showRew").disabled=true;
+	document.getElementById("preloadRew").disabled=true;
+    TMAds.showInterstitialAd();
+  };
+  document.getElementById("preloadInt").onclick=function(){ //preloading TMAds Interstital upon clicking "Preload" button
+    this.disabled=true;
+    document.getElementById("title").innerHTML="Preloading Interstital...";
+    document.getElementById("showInt").disabled=true;
+    document.getElementById("showRew").disabled=true;
+	document.getElementById("preloadRew").disabled=true;
+    TMAds.checkInterstitialAd();
+  };
+  document.getElementById("showRew").onclick=function(){ //show TMAds Rewarded upon clicking "Show" button
+    this.disabled=true;
+    document.getElementById("title").innerHTML="Showing Rewarded...";
+    document.getElementById("preloadRew").disabled=true;
+    document.getElementById("showInt").disabled=true;
+	document.getElementById("preloadInt").disabled=true;
+    TMAds.showRewardedAd();
+  };
+  document.getElementById("preloadRew").onclick=function(){ //preloading TMAds Rewarded upon clicking "Preload" button
+    this.disabled=true;
+    document.getElementById("title").innerHTML="Preloading Rewarded...";
+    document.getElementById("showRew").disabled=true;
+    document.getElementById("showInt").disabled=true;
+	document.getElementById("preloadInt").disabled=true;
+    TMAds.checkRewardedAd();
+  };
   TMAds.addCallback("onAdLoadSuccess",function(obj){ //adding on TMAds ad loaded callback
     document.getElementById("title").innerHTML="Banner loaded, height: "+obj.banner_height.toString()+"px";
     if (obj.banner_position=="bottom") //if banner is positioned at the bottom, padding the page contents a bit so it won't be covered by banner
@@ -26,5 +58,61 @@ window.onload=function(){ //initializing the whole thing on page load
     document.getElementById("hide").disabled=true;
     document.getElementById("show").disabled=false;
     document.getElementById("pos").disabled=false;
+  });
+  TMAds.addCallback("onInterstitialAdLoadSuccess",function(){ //adding on TMAds interstitial ad load success callback
+    document.getElementById("title").innerHTML="Interstitial loaded";
+    document.getElementById("preloadInt").disabled=true;
+    document.getElementById("showInt").disabled=false;
+    document.getElementById("showRew").disabled=false;
+	document.getElementById("preloadRew").disabled=false;
+  });
+  TMAds.addCallback("onInterstitialAdLoadFail",function(){ //adding on TMAds interstitial ad load fail callback
+    document.getElementById("title").innerHTML="Interstitial failed to load";
+    document.getElementById("preloadInt").disabled=false;
+    document.getElementById("showInt").disabled=false;
+    document.getElementById("showRew").disabled=false;
+	document.getElementById("preloadRew").disabled=false;
+  });
+  TMAds.addCallback("onInterstitialAdShowSuccess",function(){ //adding on TMAds interstitial ad show success callback
+    document.getElementById("title").innerHTML="Interstitial successfully showed";
+    document.getElementById("preloadInt").disabled=false;
+    document.getElementById("showInt").disabled=false;
+    document.getElementById("showRew").disabled=false;
+	document.getElementById("preloadRew").disabled=false;
+  });
+  TMAds.addCallback("onInterstitialAdShowFail",function(){ //adding on TMAds interstitial ad show fail callback
+    document.getElementById("title").innerHTML="Interstitial failed to show";
+    document.getElementById("preloadInt").disabled=false;
+    document.getElementById("showInt").disabled=false;
+    document.getElementById("showRew").disabled=false;
+	document.getElementById("preloadRew").disabled=false;
+  });
+  TMAds.addCallback("onRewardedAdLoadSuccess",function(){ //adding on TMAds Rewarded ad load success callback
+    document.getElementById("title").innerHTML="Rewarded loaded";
+    document.getElementById("preloadInt").disabled=false;
+    document.getElementById("showInt").disabled=false;
+    document.getElementById("showRew").disabled=false;
+	document.getElementById("preloadRew").disabled=true;
+  });
+  TMAds.addCallback("onRewardedAdLoadFail",function(){ //adding on TMAds Rewarded ad load fail callback
+    document.getElementById("title").innerHTML="Rewarded failed to load";
+    document.getElementById("preloadInt").disabled=false;
+    document.getElementById("showInt").disabled=false;
+    document.getElementById("showRew").disabled=false;
+	document.getElementById("preloadRew").disabled=false;
+  });
+  TMAds.addCallback("onRewardedAdShowSuccess",function(){ //adding on TMAds Rewarded ad show success callback
+    document.getElementById("title").innerHTML="Rewarded successfully showed";
+    document.getElementById("preloadInt").disabled=false;
+    document.getElementById("showInt").disabled=false;
+    document.getElementById("showRew").disabled=false;
+	document.getElementById("preloadRew").disabled=false;
+  });
+  TMAds.addCallback("onRewardedAdShowFail",function(){ //adding on TMAds Rewarded ad show fail callback
+    document.getElementById("title").innerHTML="Rewarded failed to show";
+    document.getElementById("preloadInt").disabled=false;
+    document.getElementById("showInt").disabled=false;
+    document.getElementById("showRew").disabled=false;
+	document.getElementById("preloadRew").disabled=false;
   });
 };
